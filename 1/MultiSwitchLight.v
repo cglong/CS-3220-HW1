235c0e1b0f37d04a25895b856c14576f87344c9a
Lee Benson Martin <lmartin41@gatech.edu>
2013-01-27 17:57:10 -0500

    question 1
    i believe u forgot to import the .csv file and u had the wrong device
    number set.  thx for setting up gitignore

--- 1/MultiSwitchLight.v
+++ 1/MultiSwitchLight.v
@@ -1,12 +1,17 @@
 module MultiSwitchLight(LEDG, KEY);
-	output [0:0] LEDG;
+	output [1:0] LEDG;
 	input [3:0] KEY;
 	
-	wire flip1 = !KEY[2];
-	wire flip2 = !KEY[3];
+	wire flip0 = !KEY[0];
+	wire flip1 = !KEY[1];
+	wire flip2 = !KEY[2];
+	wire flip3 = !KEY[3];
 	
-	reg state1, state2;
+	reg state0, state1, state2, state3;
+	always @(posedge flip0) state0 <= !state0;
 	always @(posedge flip1) state1 <= !state1;
 	always @(posedge flip2) state2 <= !state2;
-	assign LEDG[0] = state1 ^ state2;
+	always @(posedge flip3) state3 <= !state3;
+	assign LEDG[0] = state0 ^ state1 ^ state2 ^ state3;
+	assign LEDG[1] = state0 ^ state1 ^ state2 ^ state3;
 endmodule
