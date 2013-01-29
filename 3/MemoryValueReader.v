6ba959cf4425c4c0209406e25ff7cc5bbecd97f0
Lee Benson Martin <lmartin41@gatech.edu>
2013-01-29 18:44:37 -0500

    more complete -- but not quite working yet
    any thoughts? see any bugs?

--- 3/MemoryValueReader.v
+++ 3/MemoryValueReader.v
@@ -1,12 +1,23 @@
-module MemoryValueReader();
+module MemoryValueReader(HEX0, HEX1, HEX2, HEX3, CLOCK_50);
+	(* ram_init_file = "3.mif" *)
+	output [7:0] HEX0, HEX1, HEX2, HEX3; 	
+	input CLOCK_50;
 	reg [15:0] mem[0:1023]; // 1024-entry, 16-bit memory
 	reg [15:0] mdr; // 16-bit MDR register
 	reg [9:0] mar; // 10-bit MAR register
+	reg [31:0] Cnt;
 	initial mar = 10'd0; // MAR starts with value zero
-	always @(posedge clock) begin
-	mdr <= mem[mar]; // Read memory
-	mar <= mar + 10’d1; // And increment mar register
+	initial Cnt = 32'd0;
+	
+	always @(posedge CLOCK_50) begin
+		Cnt <= Cnt + 32'd1;
+		if (Cnt == 32'd25000000) begin
+			mdr <= mem[mar]; // Read memory
+			mar <= mar + 10'd1; // And increment mar register
+			Cnt <= 32'd0;
+		end
 	end
+	
 	// Do something with MDR, e.g. display it:
 	SevenSeg sseg0(.IN(mdr[ 3: 0]),.OUT(HEX0));
 	SevenSeg sseg1(.IN(mdr[ 7: 4]),.OUT(HEX1));
